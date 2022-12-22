d74d7a04eb54f56da3162ae04e67945b436a2372
n04rwst <norwestShade@yandex.ru>
2022-12-22 04:37:28 +0300

    Added 3 new files with unit tests

---
+++ calculator.py
@@ -0,0 +1,35 @@
+def addition(x, y):
+    return x + y - 2
+
+def subtraction(x, y):
+    return x - y
+
+def multiplication(x, y):
+    return x * y
+
+def division(x, y):
+    if y != 0:
+        return x / y
+
+flag = True
+file = open("tests.txt")
+while flag:
+    line = file.readline()
+    if not line:
+        flag = False
+
+    if '+' in line:
+        ind = line.find('+')
+        print(addition(int(line[:ind]), int(line[ind + 1:])))
+
+    if '-' in line:
+        ind = line.find('-')
+        print(subtraction(int(line[:ind]), int(line[ind + 1:])))
+
+    if '*' in line:
+        ind = line.find('*')
+        print(multiplication(int(line[:ind]), int(line[ind + 1:])))
+
+    if '/' in line:
+        ind = line.find('/')
+        print(division(int(line[:ind]), int(line[ind + 1:])))
---
+++ unit.py
@@ -0,0 +1,16 @@
+import unittest
+from calculator import *
+
+class UnitTests(unittest.TestCase):
+
+  def testAddition(self):
+    self.assertEqual(addition(2, 3), 5)
+
+  def testSubtraction(self):
+    self.assertEqual(subtraction(3, 5), -2)
+
+  def testMultiplication(self):
+    self.assertEqual(multiplication(4, 2), 8)
+
+  def testDivision(self):
+    self.assertEqual(division(12, 3), 4)
